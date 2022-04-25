001150e0fa31f332c1134d2f82cf35ae28eddb19
Shinnybest <kim295521@naver.com>
2022-04-25 20:41:57 +0900

    .

--- programmers/로또의최고최저순위.py
+++ programmers/로또의최고최저순위.py
@@ -18,3 +18,27 @@ def solution(lottos, win_nums):
         answer = [same, same]
         
     return answer
+
+# 다른 사람의 풀이, index, count
+def solution_1(lottos, win_nums):
+
+    rank=[6,6,5,4,3,2,1]
+
+    cnt_0 = lottos.count(0)
+    ans = 0
+    for x in win_nums:
+        if x in lottos:
+            ans += 1
+    return rank[cnt_0 + ans],rank[ans]
+
+def solution_2(lottos, win_nums):
+    rank = {
+        0: 6,
+        1: 6,
+        2: 5,
+        3: 4,
+        4: 3,
+        5: 2,
+        6: 1
+    }
+    return [rank[len(set(lottos) & set(win_nums)) + lottos.count(0)], rank[len(set(lottos) & set(win_nums))]]
