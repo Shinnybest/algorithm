c1afacca6de01e82ba83ad9ab0a23b5c3f30df76
Shinnybest <kim295521@naver.com>
2022-05-15 00:36:13 +0900

    .

--- programmers/영어끝말잇기.py
+++ programmers/영어끝말잇기.py
@@ -1,49 +1,18 @@
-def solution(n, words):
-    dict = {}
-    for i in range(2, n+1):
-        dict[i] = list()
-    
-    words_list = ["dummy"] + words
-    for i in range(1, len(words)+1):
-        if i%n == 0:
-            dict[n] = words_list[i]
-        else:
-            print(i)
-            dict[i%n] = words_list[i]
-            
-    answer = []
-    
-    for i in range(len(words_list)-1):
-        if words_list[i] != words_list[i+1]:
-            if (i+1) % n == 0:
-                    print("해당내용", words_list[i+1])
-                    index = dict[n].index(words_list[i+1])
-                    answer.append[n, index]
-            else:
-                print(i+1)
-                print("해당내용", words_list[i+3])
-                print((i+1) % n)
-                print(dict[(i+1) % n])
-                print(dict[i+1])
-                index = dict[(i+1) % n].index(words_list[i+3])
-                answer.append[(i+1) % n, index]
-                
-        else:
-            return [0, 0]
-    
-    already = []
-    
-    for i in range(len(words)):
-        if words[i] in already:
-            if (i) % n == 0:
-                    index = dict[n].index()
-                    answer.append[n, index]
-    
-            else:
-                index = dict[(i) % n].index()
-                answer.append[(i) % n, index]
-        else:
-            already.append(words[i])
-    return answer
+from collections import defaultdict
 
-solution(3, ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"])
+def solution(n, words):
+    words = [words[0][0]] + words
+    dict=defaultdict(list)
+    a = len(words) // n
+    for i in range(a):
+        for j in range(1, n+1):
+            if (i*n + j) == len(words):
+                return [0, 0]
+            b = words[i*n + j]
+            dict[j].append(b)
+            if b in words[:i*n + j] or b[0] != words[i*n + j - 1][-1]:
+                return [j, len(dict[j])]
+    return [0, 0]
+
+
+solution(2, ["hello", "one", "even", "never", "now", "world", "draw"])
