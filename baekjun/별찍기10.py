84b9c90460eb10cab756ec1e1de143fc6251b476
Shinnybest <kim295521@naver.com>
2022-06-06 14:44:07 +0900

    완성

--- baekjun/별찍기10.py
+++ baekjun/별찍기10.py
@@ -1,21 +1,18 @@
-# M = int(input())
-M = 27
+M = int(input())
 
 graph = [['*' for j in range(M)] for i in range(M)]
 
-def empty(size, i, j):
-    return
-
+def empty(size, x, y):
+    for i in range(x+size//3, x+2*size//3):
+        for j in range(y+size//3, y+2*size//3):
+            graph[i][j]=" "
 
 size = 3
 while size<=M:
     for i in range(0, M, size):
         for j in range(0, M, size):
             empty(size, i, j)
-
     size = size*3
-
-
 
 for i in range(M):
     str = ''.join(graph[i])
